1c22e1ed99ef04e9c0221621e1f1b651d778cd94
morph3o <morph3o@gmail.com>
2016-09-16 11:48:15 +0200

    Added Factorial

--- src/MyModule.scala
+++ src/MyModule.scala
@@ -1,5 +1,17 @@
 object MyModule {
 
+  def factorial(n: Int): Int = {
+    def go(n: Int, acc: Int): Int =
+      if(n <= 0) acc
+      else go(n - 1, n * acc) //This is in tail position as it returns the value of the recursive call directly
+    go(n, 1)
+  }
+
+  private def formatFactorial(n: Int): String = {
+    val msg = "The factorial of %d is %d"
+    msg.format(n, factorial(n))
+  }
+
   def abs(n: Int): Int = {
     if(n < 0) -n
     else n
@@ -10,7 +22,9 @@ object MyModule {
     msg.format(x, abs(x))
   }
 
-  def main(args: Array[String]): Unit =
+  def main(args: Array[String]): Unit = {
     println(formatAbs(42))
+    println(formatFactorial(7))
+  }
 
 }
