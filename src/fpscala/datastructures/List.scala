4ddcc1d3587b60d8ccabb7b947200c6e50a32027
Piero Divasto <pdivasto@gmail.com>
2016-09-18 17:18:57 +0200

    Added trait and sealed

--- src/fpscala/datastructures/List.scala
+++ src/fpscala/datastructures/List.scala
@@ -1,8 +1,7 @@
 package fpscala.datastructures
 
-/**
-  * Created by pdivasto on 18-09-2016.
-  */
-class List {
-
-}
+/*
+  - Declaring the abstract interface List with no methods.
+  - Sealed means that the implementation of the trait must be in this file.
+ */
+sealed trait List[+A]
