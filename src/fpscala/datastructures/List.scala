b60ff4d98473a3fc38c789621674d1496c4fd418
Piero Divasto <pdivasto@gmail.com>
2016-09-19 11:24:42 +0200

    Added comments

--- src/fpscala/datastructures/List.scala
+++ src/fpscala/datastructures/List.scala
@@ -51,10 +51,11 @@ object List {
     if (as.isEmpty) Nil
     else Cons(as.head, apply(as.tail: _*))
 
+  // This value of x = 3
   val x = List(1,2,3,4,5) match {
     case Cons(x, Cons(2, Cons(4, _))) => x
     case Nil => 42
-    case Cons(x, Cons(y, Cons(3, Cons(4, _)))) => x + y
+    case Cons(x, Cons(y, Cons(3, Cons(4, _)))) => x + y // This is the case fulfilled
     case _ => 101
   }
 
